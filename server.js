1fb9155fe14eb7c66d2281ddeec52a80aeb19c8d
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-09-18 13:00:19 +0200

    Modify server.js to make folder public visible

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.use('/api', adsRoutes);
 app.use('/auth', authRoutes);
 
 app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
