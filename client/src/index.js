99b4059d1902f7163445fec1fc1f95bf5ace6289
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-09-27 14:20:11 +0200

    Add bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import "./styles/global.scss";
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import { BrowserRouter as Router } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const container = document.querySelector('#root');
 const root = createRoot(container);
