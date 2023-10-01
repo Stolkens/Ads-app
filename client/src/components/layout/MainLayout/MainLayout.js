fec6a2d3932751ac2fafc50dc3e88b9226e62ba6
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-10-01 12:22:53 +0200

    Modify adsRedux

--- client/src/components/layout/MainLayout/MainLayout.js
+++ client/src/components/layout/MainLayout/MainLayout.js
@@ -1,14 +1,20 @@
 import Footer from "../Footer/Footer";
 import MainMenu from "../MainMenu/MainMenu";
 import styles from './MainLayout.module.scss';
+import PropTypes from 'prop-types';
 
-const MainLayout = () => {
+const MainLayout = ({children}) => {
   return (
     <div className={styles.container}>
       <MainMenu />
+      {children}
       <Footer />
     </div>
   );
 }
+
+MainLayout.propTypes = {
+  children: PropTypes.node,
+};
 
 export default MainLayout;
--- client/src/redux/adsRedux.js
+++ client/src/redux/adsRedux.js
@@ -1,47 +1,37 @@
 import { API_URL } from "../config";
 /* SELECTORS */
-export const getAds = ({ ads }) => ads.data;
-export const getRequest = ({ ads }) => ads.request;
+export const getAds = ({ ads }) => ads;
 
 /* ACTIONS */
+const reducerName = 'ads';
+const createActionName = (name) => `app/${reducerName}/${name}`;
 
-const createActionName = (name) => `app/ads/${name}`;
-const START_REQUEST = createActionName("START_REQUEST");
-const END_REQUEST = createActionName("END_REQUEST");
-const ERROR_REQUEST = createActionName("ERROR_REQUEST");
 const LOAD_ADS = createActionName("LOAD_ADS");
 const ADD_AD = createActionName("ADD_AD");
-export const startRequest = () => ({ type: START_REQUEST });
-export const endRequest = () => ({ type: END_REQUEST });
-export const errorRequest = (error) => ({ error, type: ERROR_REQUEST });
-export const loadAds = (payload) => ({ payload, type: LOAD_ADS });
-export const addAd = (payload) => ({ payload, type: ADD_AD });
+
+export const loadAds = (payload) => ({ type: LOAD_ADS, payload });
+export const addAd = (payload) => ({ type: ADD_AD, payload });
 
 /* THUNKS */
 export const loadAdsRequest = () => {
   return async (dispatch) => {
-    dispatch(startRequest({ name: "LOAD_ADS" }));
+    dispatch(loadAds(null));
 
     try {
       let response = await fetch(`${API_URL}/ads`);
-      
-      if (!response.ok) {
-        throw new Error(`Request failed with status: ${response.status}`);
-      }
-
       let data = await response.json();
       dispatch(loadAds(data));
-      dispatch(endRequest({ name: "LOAD_ADS" }));
+      console.log('data', data);
+      console.log('response', response);
+      
     } catch (error) {
-      dispatch(errorRequest({ name: "LOAD_ADS", error: error.message }));
+      console.log(error);
     }
   };
 };
 
 export const addAdRequest = (ad) => {
   return async (dispatch) => {
-    dispatch(startRequest({ name: "ADD_AD" }));
-
     try {
       let response = await fetch(`${API_URL}/ads`, {
         method: 'POST',
@@ -57,56 +47,22 @@ export const addAdRequest = (ad) => {
 
       let data = await response.json();
       dispatch(addAd(data));
-      dispatch(endRequest({ name: "ADD_AD" }));
+     
     } catch (error) {
-      dispatch(errorRequest({ name: "ADD_AD", error: error.message }));
+      console.log(error);
     }
   };
 };
 
-/* INITIAL STATE */
-
-const initialState = {
-  data: [],
-  requests: {},
-};
 
 /* REDUCER */
 
-const adsReducer = (statePart = initialState, action = {}) => {
+const adsReducer = (statePart = [], action = {}) => {
   switch (action.type) {
     case LOAD_ADS:
-      return { ...statePart, data: [...action.payload] };
+      return [...action.payload];
     case ADD_AD:
-      return { ...statePart, data: [...statePart.data, action.payload] };
-    case START_REQUEST:
-      return {
-        ...statePart,
-        requests: {
-          ...statePart.requests,
-          [action.payload.name]: { pending: true, error: null, success: false },
-        },
-      };
-    case END_REQUEST:
-      return {
-        ...statePart,
-        requests: {
-          ...statePart.requests,
-          [action.payload.name]: { pending: false, error: null, success: true },
-        },
-      };
-    case ERROR_REQUEST:
-      return {
-        ...statePart,
-        requests: {
-          ...statePart.requests,
-          [action.payload.name]: {
-            pending: false,
-            error: action.payload.error,
-            success: false,
-          },
-        },
-      };
+      return [...statePart, action.payload];
     default:
       return statePart;
   }
